8c7ce2ed96b7a863f385304f7407f566306b0813
Sajjad Momeni <sajjad5221@gmail.com>
2023-10-23 11:49:01 +0330

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Footer from '@/components/layout/Footer';
+import './globals.css';
+import Navbar from '@/components/layout/Navbar';
+import ScrollUpButton from '@/components/common/ScrollUpButton';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>
+        <Navbar> {/* Navbar */}
+          <main>{children}</main> {/* Main Content */}
+          <div className="bottom-0 w-full">
+            <Footer /> {/* Footer */}
+            <ScrollUpButton /> {/* Scroll Up Button */}
+          </div>
+        </Navbar>
+      </body>
+    </html>
+  );
+}
