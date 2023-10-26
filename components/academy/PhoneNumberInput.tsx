9def33ae2f2beeb1f7fcf2717245138c4cd4c382
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-10-27 01:01:08 +0330

    Chore: Create phone input

---
+++ components/academy/PhoneNumberInput.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useState } from "react";
+import PhoneInputWithCountrySelect from "react-phone-number-input";
+import "react-phone-number-input/style.css";
+
+export default function PhoneInput() {
+  const [phone, setPhone] = useState("");
+
+  return (
+    <div>
+      <PhoneInputWithCountrySelect
+        value={phone}
+        onChange={() => setPhone(phone)}
+        defaultCountry="IR"
+        international
+        countryCallingCodeEditable={false}
+        className="w-[360px] md:w-[389px] h-[40px] md:h-[50px] pl-5 bg-yellow-50 border border-yellow-400 justify-start items-center inline-flex [&>*:nth-child(2)]:bg-yellow-50 !important] [&>*:nth-child(1)]:border-r-2 !important]"
+      />
+    </div>
+  );
+}
