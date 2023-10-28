62c11c9a2aeb76206d4b0c8af406a0cc0a4b4686
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-10-28 14:02:05 +0330

    Chore: Fix hero

--- components/academy/Hero.tsx
+++ components/academy/Hero.tsx
@@ -1,9 +1,9 @@
-'use client';
-import Image from 'next/image';
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
-import Button from '../common/Button';
-import ArrowDown from '../icons/hero/ArrowDown';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import Button from "../common/Button";
+import ArrowDown from "../icons/hero/ArrowDown";
 
 export default function Hero({
   showLanda,
@@ -23,55 +23,44 @@ export default function Hero({
   showButton?: boolean;
 }) {
   return (
-    <div className="h-[850px] relative overflow-hidden bg-[#3D5656]">
+    <div className="flex h-[calc(100vh-144px)] justify-between mt-36 pb-20 bg-white">
       <Image
         loading="lazy"
-        className="absolute bottom-16 hidden md:block"
-        src={'/static/images/Academy/Hero/leadership-amico 1.svg'}
-        width={383}
-        height={383}
+        className="hidden md:block w-1/3 object-contain"
+        src={"/static/images/Academy/Hero/Hero.png"}
+        width={481}
+        height={501}
         alt="Hero Illustration"
       />
       <Image
         loading="lazy"
-        className="absolute bottom-16 md:hidden"
-        src={'/static/images/Academy/Hero/leadership-amico 1.svg'}
+        className="md:hidden"
+        src={"/static/images/Academy/Hero/leadership-amico 1.svg"}
         width={214}
         height={214}
         alt="Hero Illustration"
       />
-      <div className="flex flex-col items-center justify-center md:h-screen space-y-5 py-36">
-        <div className="md:w-[712px] flex flex-col items-center space-y-4 px-10">
-          <div className="font-gilda text-4xl md:text-6xl text-[#F1F8EC]">
+      <div className="flex flex-col ml-6 w-2/3">
+        <div className="flex flex-col">
+          <div className="font-gilda text-4xl md:text-6xl text-black">
             {title}
           </div>
-          <div className="font-condensed text-neutral-50 text-opacity-95 text-xl md:text-2xl font-semibold leading-normal text-center md:leading-10 tracking-[2px] ">
+          <div className="w-[800px] font-condensed text-black text-opacity-95 text-xl md:text-2xl font-semibold leading-normal md:leading-10 tracking-[2px] ">
             {subTitle}
           </div>
         </div>
-        {showButton ? (
-          <Button
-            text="Register Now"
-            size="notVisit"
-            bgColor="Yellow"
-            addedClass={buttonBg ? 'md:hidden' : 'block'}
-            goto="/"
-          />
-        ) : (
-          <></>
-        )}
         <Image
           loading="lazy"
-          className="w-[300px] md:w-[412px] h-[400px] md:h-[560px] absolute -right-28 md:right-0 -bottom-20"
-          src={`/static/images/${leftImage}`}
+          className="h-[400px] md:h-[560px] self-end"
+          src={`/static/images/Academy/Logo.svg`}
           alt="Landa"
-          width={412}
-          height={500}
+          width={344}
+          height={467}
         />
         <Link
           aria-label="Scroll To Next Section"
           className="absolute flex items-center justify-center w-10 h-10 md:w-12 md:h-12 border border-white rounded-full animate-bounce left-7 md:left-10 bottom-4"
-          href={'#Roadmap-1'}
+          href={"#Roadmap-1"}
         >
           <ArrowDown />
         </Link>
