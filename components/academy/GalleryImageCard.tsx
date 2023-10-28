b3af9d974bd7f5884edb7357d9c876ba510d0750
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-10-28 16:29:47 +0330

    Chore: Add gallery images hover

--- components/academy/GalleryImageCard.tsx
+++ components/academy/GalleryImageCard.tsx
@@ -1,15 +1,19 @@
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 export default function GalleryImageCard({ src }: { src: string }) {
   return (
-    <div className="relative w-[170px] md:w-[555px] h-[100px] md:h-[416px]">
+    <div className="relative w-[170px] md:w-[555px] h-[100px] md:h-[416px] group">
       <Image
         className="object-cover rounded-sm"
         src={src}
         alt="Landa Academy Image"
         layout="fill"
       />
+      <div className="flex-col text-white border-t-2 border-white absolute bottom-7 pl-24 pt-2 z-10 w-full hidden group-hover:flex fade-in">
+        <span className="font-barlow text-xl font-medium">Desert Tour</span>
+        <span className="font-barlow text-sm font-medium">20 Jan 2023</span>
+      </div>
     </div>
   );
 }
