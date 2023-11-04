17cd3ab2c890875ee8aac6fd16f21e730dd65757
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-11-04 11:14:18 +0330

    Chore: Apply fixes to scroll menu

--- components/academy/ScrollMenu.tsx
+++ components/academy/ScrollMenu.tsx
@@ -1,64 +1,101 @@
-'use client';
-import React, { useRef, useState } from 'react';
-import CourseCard from './CourseCard';
-import ArrowUp from '../icons/common/ArrowUp';
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import CourseCard from "./CourseCard";
+import ArrowUp from "../icons/common/ArrowUp";
 
 export default function ScrollMenu() {
   const scrollContainerRef = useRef<HTMLDivElement | null>(null);
-  const [scrollPosition, setScrollPosition] = useState(0);
+  const [isScrolling, setIsScrolling] = useState(true); // Control scrolling with a Boolean state
 
-  const handleScroll = (scrollOffset: number) => {
-    const container = scrollContainerRef.current;
-    if (container) {
-      const newPosition = scrollPosition + scrollOffset;
-      container.scrollLeft = newPosition;
-      setScrollPosition(newPosition);
+  useEffect(() => {
+    const scrollContainer = scrollContainerRef.current;
+
+    function scrollAutomatically() {
+      const scrollAmount = 1;
+      if (scrollContainer != null && isScrolling) {
+        // Check if scrolling is allowed
+        const isScrollingLeft = scrollContainer.scrollLeft > 0;
+
+        if (!isScrollingLeft) {
+          scrollContainer.scrollLeft = scrollContainer.scrollWidth;
+        } else {
+          scrollContainer.scrollLeft -= scrollAmount;
+        }
+      }
+    }
+
+    const intervalId = setInterval(scrollAutomatically, 80); // Adjust the interval as needed.
+
+    return () => clearInterval(intervalId);
+  }, [isScrolling]); // Add isScrolling as a dependency
+
+  // Add event listeners to stop automatic scroll on mouse enter
+  const handleMouseEnter = () => {
+    setIsScrolling(false); // Stop scrolling
+  };
+
+  // Add event listener to resume automatic scroll on mouse leave
+  const handleMouseLeave = () => {
+    setIsScrolling(true); // Resume scrolling
+  };
+
+  // Scroll function to handle both left and right scroll
+  const handleScroll = (direction: "left" | "right") => {
+    const scrollContainer = scrollContainerRef.current;
+    if (scrollContainer) {
+      const scrollAmount = 200; // Adjust the scroll amount as needed
+      if (direction === "left") {
+        scrollContainer.scrollLeft -= scrollAmount;
+      } else {
+        scrollContainer.scrollLeft += scrollAmount;
+      }
     }
   };
 
   const cardData = [
     {
-      title: 'Business',
-      image: '/static/images/Academy/courses/Rectangle 3014 (2).png',
-      date: '27/Dec/2023',
+      title: "Business",
+      image: "/static/images/Academy/courses/Rectangle 3014 (2).png",
+      date: "27/Dec/2023",
     },
     {
-      title: 'AI webinar',
-      image: '/static/images/Academy/courses/Rectangle 3011.png',
-      date: '27/Dec/2023',
+      title: "AI webinar",
+      image: "/static/images/Academy/courses/Rectangle 3011.png",
+      date: "27/Dec/2023",
     },
     {
-      title: 'SEO course',
-      image: '/static/images/Academy/courses/Rectangle 3014.png',
-      date: '27/Dec/2023',
+      title: "SEO course",
+      image: "/static/images/Academy/courses/Rectangle 3014.png",
+      date: "27/Dec/2023",
 
       active: true,
     },
     {
-      title: 'Python course',
-      image: '/static/images/Academy/courses/Rectangle 3016.png',
-      date: '27/Dec/2023',
+      title: "Python course",
+      image: "/static/images/Academy/courses/Rectangle 3016.png",
+      date: "27/Dec/2023",
     },
     {
-      title: 'UX Workshop',
-      image: '/static/images/Academy/courses/Rectangle 3015.png',
-      date: '27/Dec/2023',
+      title: "UX Workshop",
+      image: "/static/images/Academy/courses/Rectangle 3015.png",
+      date: "27/Dec/2023",
     },
     {
-      title: 'Icdl workshop',
-      image: '/static/images/Academy/courses/Rectangle 3011 (1).png',
-      date: '27/Dec/2023',
+      title: "Icdl workshop",
+      image: "/static/images/Academy/courses/Rectangle 3011 (1).png",
+      date: "27/Dec/2023",
     },
     {
-      title: 'Front-end course',
+      title: "Front-end course",
       image:
-        '/static/images/Academy/courses/027fae0dc46b4eacee7dbeeda27b2fdb.jpg',
-      date: '27/Dec/2023',
+        "/static/images/Academy/courses/027fae0dc46b4eacee7dbeeda27b2fdb.jpg",
+      date: "27/Dec/2023",
     },
     {
-      title: 'Digital marketing',
-      image: '/static/images/Academy/courses/',
-      date: '27/Dec/2023',
+      title: "Digital marketing",
+      image:
+        "/static/images/Academy/courses/027fae0dc46b4eacee7dbeeda27b2fdb.jpg",
+      date: "27/Dec/2023",
     },
   ];
 
@@ -73,14 +110,16 @@ export default function ScrollMenu() {
         </div>
         <div className="flex items-center mx-5">
           <button
-            className="bg-gray-700 text-white md:px-4 md:py-2 p-2 md:rounded-l rounded-full"
-            onClick={() => handleScroll(-200)} // Adjust scroll offset as needed
+            className="bg-gray-700 text-white md:px-4 md:py-2 p-2 md:rounded-r rounded-full"
+            onClick={() => handleScroll("left")} // Adjust scroll offset as needed
           >
             <ArrowUp />
           </button>
           <div
             className="flex overflow-x-scroll md:overflow-x-hidden whitespace-nowrap mx-2 gap-4 md:gap-12 md:h-[486px]"
             ref={scrollContainerRef}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
           >
             {cardData.map((card, index) => (
               <CourseCard
@@ -93,8 +132,8 @@ export default function ScrollMenu() {
             ))}
           </div>
           <button
-            className="bg-gray-700 text-white md:px-4 md:py-2 p-2 md:rounded-r rounded-full"
-            onClick={() => handleScroll(200)} // Adjust scroll offset as needed
+            className="bg-gray-700 text-white md:px-4 md:py-2 p-2 md:rounded-l rounded-full"
+            onClick={() => handleScroll("right")} // Adjust scroll offset as needed
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
