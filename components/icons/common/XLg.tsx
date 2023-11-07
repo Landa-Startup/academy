f1054bf79977c61e36ef7869e1f73d9ca591fb0a
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-11-07 11:07:12 +0330

    Init: X icon

---
+++ components/icons/common/XLg.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function XLg({ onClick }: { onClick: () => void }) {
+  return (
+    <svg
+      onClick={onClick}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="currentColor"
+      className="bi bi-x-lg cursor-pointer hover:text-yellow-400"
+      viewBox="0 0 16 16"
+    >
+      <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+    </svg>
+  );
+}
