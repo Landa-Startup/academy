4c8270cb808b2bb13e90203baa2fc33904b6be36
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-11-07 15:44:34 +0330

    Chore: Add SEO course page with banner and course details

--- app/courses/[slug]/page.tsx
+++ app/courses/[slug]/page.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import SeoCourses from "@/components/academy/SeoCourses";
+import React from "react";
 
 export default function page() {
   return (
-    <div>page</div>
-  )
+    <div>
+      <SeoCourses />
+    </div>
+  );
 }
