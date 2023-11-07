b2d3d9732440f160566da8f2668ee93731f5dc22
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-11-07 12:23:15 +0330

    Chore: Add XLg component to common icons, create PhotoModal component, and update GalleryImageCard to use PhotoModal

--- components/academy/GalleryImageCard.tsx
+++ components/academy/GalleryImageCard.tsx
@@ -1,19 +1,46 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import PhotoModal from "../PhotoModal";
 
 export default function GalleryImageCard({ src }: { src: string }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const openModal = (imageUrl: any) => {
+    setSelectedImage(imageUrl);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div className="relative w-[170px] md:w-[555px] h-[100px] md:h-[416px] group">
-      <Image
-        className="object-cover rounded-sm"
-        src={src}
-        alt="Landa Academy Image"
-        layout="fill"
-      />
-      <div className="flex-col text-white border-t-2 border-white absolute bottom-7 pl-24 pt-2 z-10 w-full hidden group-hover:flex fade-in">
-        <span className="font-barlow text-xl font-medium">Desert Tour</span>
-        <span className="font-barlow text-sm font-medium">20 Jan 2023</span>
+    <>
+      <div
+        className="relative w-[170px] md:w-[555px] h-[100px] md:h-[416px] group cursor-pointer"
+        onClick={() => openModal(src)}
+      >
+        <Image
+          className="object-cover rounded-sm"
+          src={src}
+          alt="Landa Academy Image"
+          layout="fill"
+        />
+        <div className="flex-col text-white border-t-2 border-white absolute bottom-7 pl-24 pt-2 z-10 w-full hidden group-hover:flex fade-in">
+          <span className="font-barlow text-xl font-medium">Desert Tour</span>
+          <span className="font-barlow text-sm font-medium">20 Jan 2023</span>
+        </div>
       </div>
-    </div>
+
+      <PhotoModal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        imageUrl={selectedImage}
+        closeModal={closeModal}
+      />
+    </>
   );
 }
--- components/icons/common/XLg.tsx
+++ components/icons/common/XLg.tsx
@@ -8,7 +8,7 @@ export default function XLg({ onClick }: { onClick: () => void }) {
       width="24"
       height="24"
       fill="currentColor"
-      className="bi bi-x-lg cursor-pointer hover:text-yellow-400"
+      className="bi bi-x-lg cursor-pointer mb-2 hover:text-yellow-400"
       viewBox="0 0 16 16"
     >
       <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
