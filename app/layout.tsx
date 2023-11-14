f41e66ab5926024c20c834def7e5a89fb354a9a2
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-11-14 10:46:31 +0330

    Chore: Refactor layout component and add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import Footer from '@/components/layout/Footer';
-import './globals.css';
-import Navbar from '@/components/layout/Navbar';
-import ScrollUpButton from '@/components/common/ScrollUpButton';
+import React from "react";
+import Footer from "@/components/layout/Footer";
+import "./globals.css";
+import Navbar from "@/components/layout/Navbar";
+import ScrollUpButton from "@/components/common/ScrollUpButton";
 
 export default function RootLayout({
   children,
@@ -13,12 +13,13 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/images/favicon.ico" />
       </head>
       <body>
-        <Navbar> {/* Navbar */}
+        <Navbar>
+          {/* Navbar */}
           <main>{children}</main> {/* Main Content */}
           <div className="bottom-0 w-full">
-          
             <ScrollUpButton /> {/* Scroll Up Button */}
           </div>
         </Navbar>
