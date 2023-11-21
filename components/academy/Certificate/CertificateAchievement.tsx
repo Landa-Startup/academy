80fd3230e47de26946942fadd0c9ad8f3757e719
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-11-21 14:48:13 +0330

    chore: add CertificateCoursePromote component and update styles in CertificateAchievement.tsx

--- components/academy/Certificate/CertificateAchievement.tsx
+++ components/academy/Certificate/CertificateAchievement.tsx
@@ -1,49 +1,55 @@
+import CertificateCoursePromote from '@/components/CertificateCoursePromote';
 import Image from 'next/image';
 export default function CertificateAchievement() {
   return (
-    <div className="flex flex-col text-white h-[890px]">
-      <div className="flex flex-col md:flex-row-reverse pt-16">
-        <div className="pt-20 sm:block hidden">
-          <Image
-            src="/static/images/Academy/gallery/academy.stroke.png"
-            alt="academy"
-            width={500}
-            height={500}
-          />
-        </div>
-        <div className="flex flex-col justify-start w-[1200px]">
-          <h2 className="font-gilda text-[#FDD30A] mt-2 ml-12 text-5xl">
-            CERTIFICATE OF ACHIEVEMNENT
-          </h2>
-          <p className="text-xl mt-12 ml-12 text-[#6B6B6B] font-barlow">
-            AWARDED TO
-          </p>
-          <p className="pt-12 ml-12 text-[#3D5656] font-barlow capitalize text-5xl">
-            MOHAMAD AMIN ASGARIAN
-          </p>
-          <p className="text-[#6B6B6B] pt-12 text-xl ml-12">
+    <div className="flex flex-col justify-around text-white h-screen relative">
+      <div className="grid grid-cols-2 justify-items-center">
+        <div className="flex flex-col gap-11">
+          <div className="flex flex-col gap-5 justify-start p-10 rounded-sm bg-[#fdd40a1a]">
+            <h2 className="font-gilda text-[#FDD30A] text-4xl">
+              CERTIFICATE OF ACHIEVEMNENT
+            </h2>
+            <p className="text-lg text-[#6B6B6B] font-barlow">AWARDED TO</p>
+            <p className="text-[#3D5656] font-barlow capitalize text-4xl">
+              MOHAMAD AMIN ASGARIAN
+            </p>
+
             <p className="text-[#6B6B6B]"> For Participating in</p>
-            <p className="pt-12 font-barlow text-5xl capitalize text-[#80C555]">
+            <p className="font-barlow text-4xl capitalize text-[#80C555]">
               SEO Course
             </p>
-            <p className="w-[642px] pt-12 font-barlow text-[#6B6B6B]">
+            <p className="w-[642px] font-barlow text-[#6B6B6B]">
               The course was designed to provide participants with a
               comprehensive understanding of SEO strategies, techniques, and the
               best practices.
             </p>
-          </p>
-          <div className="pl-12 pt-12">
-            <Image
-              src="/static/images/Academy/gallery/Certificate.png"
-              alt="academy"
-              width={250}
-              height={500}
-            />
             <div>
-              <p className="text-black pt-4 font-Roboto">
+              <p className="text-[#6B6B6B] font-barlow text-sm capitalize">
                 issued on Jul/02/2022
               </p>
             </div>
+          </div>
+          <CertificateCoursePromote
+            course={'Seo Course'}
+            image="3a3225cf5a0508f377effbd0acb03f5c.jpg"
+          />
+        </div>
+        <div className="relative">
+          <div className="flex flex-col gap-5">
+            <Image
+              src="/static/images/Academy/gallery/Certificate.png"
+              alt="academy"
+              width={700}
+              height={400}
+            />
+          </div>
+          <div className="absolute -right-8 -bottom-24 -z-10">
+            <Image
+              src="/static/images/Academy/gallery/academy.stroke.png"
+              alt="academy"
+              width={500}
+              height={500}
+            />
           </div>
         </div>
       </div>
