012a13c7328e84a5402d697b52fe4c95e12ae8ca
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-11-25 15:42:20 +0330

    chore: update ESLint and TypeScript configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,53 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:tailwindcss/recommended',
+    'eslint-config-prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    extends: [
-      'next/core-web-vitals',
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      "plugin:@typescript-eslint/eslint-recommended",
-      'plugin:tailwindcss/recommended',
-      'eslint-config-prettier'
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'tailwindcss',
+    'no-loops',
+    'prettier'
+  ],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-unused-vars': 'error',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'tailwindcss/no-custom-classname': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'max-lines': [
+      'error',
+      { max: 350, skipComments: true, skipBlankLines: true }
     ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    plugins: ['react', '@typescript-eslint', 'tailwindcss','no-loops','prettier'],
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'error',
-      'react/jsx-uses-react': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      'tailwindcss/no-custom-classname': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      "max-lines": ["error", { "max": 350, "skipComments": true, "skipBlankLines": true }],
-      "components/icons/*": ["off"],
-    },
-    settings: {
-      'tailwindcss': {
-        groupByResponsive: true,
-        whitelist: [],
-      },
-    },
-  };
+    'components/icons/*': ['off']
+  },
+  settings: {
+    tailwindcss: {
+      groupByResponsive: true,
+      whitelist: []
+    }
+  }
+};
