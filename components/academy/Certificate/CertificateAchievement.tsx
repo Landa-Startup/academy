e7ab09a9d6a380af8bf523698a4bd89ec1260e5d
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-11-26 14:43:31 +0330

    chore: fix typo in CertificateAchievement component

--- components/academy/Certificate/CertificateAchievement.tsx
+++ components/academy/Certificate/CertificateAchievement.tsx
@@ -1,23 +1,39 @@
 import Certificate from '@/components/Certificate';
 import CertificateCoursePromote from '@/components/CertificateCoursePromote';
 import Image from 'next/image';
-export default function CertificateAchievement() {
+export default function CertificateAchievement({
+  name,
+  courseTitle,
+  issuedDate,
+  courseImage,
+  description,
+  qrcode_image,
+  slug
+}: {
+  name: string;
+  courseTitle: string;
+  issuedDate: string;
+  courseImage: string;
+  description: string;
+  qrcode_image: string;
+  slug: string;
+}) {
   return (
     <div className="relative flex h-screen flex-col justify-around text-white">
       <div className="grid grid-cols-2 justify-items-center">
         <div className="flex flex-col gap-11">
           <div className="flex flex-col justify-start gap-5 rounded-sm bg-[#fdd40a1a] p-10">
             <h2 className="font-gilda text-4xl text-[#FDD30A]">
-              CERTIFICATE OF ACHIEVEMNENT
+              CERTIFICATE OF ACHIEVEMENT
             </h2>
             <p className="font-barlow text-lg text-[#6B6B6B]">AWARDED TO</p>
             <p className="font-barlow text-4xl capitalize text-[#3D5656]">
-              MOHAMAD AMIN ASGARIAN
+              {name}
             </p>
 
             <p className="text-[#6B6B6B]"> For Participating in</p>
             <p className="font-barlow text-4xl capitalize text-[#80C555]">
-              SEO Course
+              {courseTitle}
             </p>
             <p className="w-[642px] font-barlow text-[#6B6B6B]">
               The course was designed to provide participants with a
@@ -26,13 +42,15 @@ export default function CertificateAchievement() {
             </p>
             <div>
               <p className="font-barlow text-sm capitalize text-[#6B6B6B]">
-                issued on Jul/02/2022
+                issued on {issuedDate}
               </p>
             </div>
           </div>
           <CertificateCoursePromote
-            course={'Seo Course'}
-            image="3a3225cf5a0508f377effbd0acb03f5c.jpg"
+            course={courseTitle}
+            image={courseImage}
+            description={description}
+            slug={slug}
           />
         </div>
         <div className="relative">
@@ -43,7 +61,11 @@ export default function CertificateAchievement() {
               width={700}
               height={400}
             /> */}
-            <Certificate />
+            <Certificate
+              name={name}
+              qrCode={qrcode_image}
+              issueDate={issuedDate}
+            />
           </div>
           <div className="absolute -bottom-24 -right-8 -z-10">
             <Image
