a8a30ce91fc5da9a317f5531a3896ce4aa4d2492
MEHDI <MehdiSalmanzadeh@outlook.com>
2023-11-26 14:42:47 +0330

    chore: refactor Certificate and CertificateCoursePromote components

--- components/Certificate.tsx
+++ components/Certificate.tsx
@@ -1,8 +1,15 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import React from 'react';
 
-export default function Certificate() {
+export default function Certificate({
+  name,
+  qrCode,
+  issueDate
+}: {
+  name: string;
+  qrCode: string;
+  issueDate: string;
+}) {
   return (
     <div className="flex h-[520px] w-[700px] items-center justify-center border-[15px] border-[#FDD30A] bg-white font-barlow text-black">
       <div className="flex h-[470px] w-[650px] flex-col items-center justify-center border-[10px] border-[#80C555]">
@@ -15,42 +22,44 @@ export default function Certificate() {
               width={50}
               height={50}
             />
-            <span className="text-xs font-semibold text-black">Landa Academy</span>
+            <span className="text-xs font-semibold text-black">
+              Landa Academy
+            </span>
           </div>
         </div>
         <span className="mt-6 text-2xl font-semibold text-black">
           Course Certificate
         </span>
-        <div className="mt-5 flex flex-col items-center gap-2 font-medium">
+        <div className="mt-5 flex flex-col items-center gap-5 font-medium">
           <span className="text-sm">This is to certify</span>
-          <span className="font-pinyon text-5xl tracking-[2.4px]">
-            Iman nasr
+          <span className="font-pinyon text-5xl font-semibold capitalize tracking-[2.4px]">
+            {name}
           </span>
-          <p className="text-xs font-medium text-[#6B6B6B]">
-            This is to certify that Iman nasr has successfully completed the SEO
-            Course conducted by Landa Business Academy
+          <p className="mx-10 text-center text-xs font-medium text-[#6B6B6B]">
+            This is to certify that{' '}
+            <span className="font-semibold capitalize">{name}</span> has
+            successfully completed the SEO Course conducted by Landa Business
+            Academy
           </p>
         </div>
-        <div className="mt-12 flex w-full justify-between px-16">
+        <div className="flex w-full justify-between px-16">
           <div className="flex flex-col items-center justify-center">
             <span className="font-sacramento text-4xl text-[#80C555]">
               moradimehr.
             </span>
             <span className="text-xs font-semibold">Founder & Instructor</span>
           </div>
-          <div className="flex flex-row items-center gap-1">
-            <div className="flex flex-col gap-2 text-xs font-semibold">
+          <div className="flex flex-col items-center gap-1">
+            {/* <div className="flex flex-col gap-2 text-xs font-semibold">
               <span>Verify Certificate:</span>
               <Link href={'/'} className="text-blue-500">
                 https://academy.landaholding.com/verify/TY65JD7b
               </Link>
-            </div>
-            <Image
-              alt="Qr code"
-              src={'/static/images/Academy/Rectangle 2981.png'}
-              width={50}
-              height={50}
-            />
+            </div> */}
+            <Image alt="Qr code" src={qrCode} width={70} height={70} />{' '}
+            <span className="bar font-barlow text-xs font-semibold">
+              Issued on: {issueDate}
+            </span>
           </div>
         </div>
       </div>
