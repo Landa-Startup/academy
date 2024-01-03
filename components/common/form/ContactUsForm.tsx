a7e3e8d1c7646d853b494b6bb6227dfbeefa85cd
amir-esfahani <ad.esfahanizadeh@yahoo.com>
2024-01-03 16:27:11 +0330

    chore: fixing styles and refactoring components

--- components/common/form/ContactUsForm.tsx
+++ components/common/form/ContactUsForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { ContactUSFormData } from '../../../app/types/global';
+import { ContactUSFormData } from '../../../types/global';
 import GetCsrfToken from '@/utils/get-csrf-token';
 import apiClient from '@/utils/api';
 import NotificationSendForm from './NotificationSendForm';
