fdc3c8e3f0281595e243b8368dc51d0f9deb797d
Sajjad Momeni <sajjad5221@gmail.com>
2024-01-06 14:56:33 +0330

    fix: fixed styles

--- components/Home/HomeGallery.tsx
+++ components/Home/HomeGallery.tsx
@@ -6,16 +6,18 @@ import { homeGalleryImage } from '../Constant/constant';
 
 export default function Gallery() {
   return (
-    <div className="relative flex h-screen snap-start flex-col justify-center my-auto gap-10 bg-neutral-50/95 py-5">
+    // h-screen snap-start bg-[#3D5656] py-10
+    <div className="relative  h-screen snap-start flex-col justify-center items-center my-auto gap-10 bg-neutral-50/95 py-16">
       <div className="lg:mb-25 flex flex-col items-center gap-3">
-        <div className="text-center font-gilda text-base font-normal tracking-[5.6px] text-lime-400  md:w-[413px]   md:text-3xl md:tracking-[11.20px]">
+        <div className="text-center font-gilda text-base font-normal tracking-[5.6px] text-lime-400  md:w-[413px] md:text-3xl md:tracking-[11.20px]">
           LANDA ACADEMY 
         </div>
         <div className="-mt-[4px] text-center font-gilda text-4xl font-normal tracking-[1.6px] text-black md:text-[64px] md:leading-[50px] md:tracking-[3.20px]">
           Our Gallery
         </div>
       </div>
-      <div className="m mx-8  mb-[184px] grid grid-cols-2 justify-items-center gap-2 md:mx-32 md:grid-cols-3 lg:mb-[-22px]">
+      <div className='flex flex-col justify-between'>
+      <div className="mx-8 grid grid-cols-2 justify-items-center gap-2 md:mx-32 md:grid-cols-3 lg:mb-[-22px]">
         {homeGalleryImage.map((image, index) => (
           <Image
             key={index}
@@ -29,8 +31,9 @@ export default function Gallery() {
           />
         ))}
       </div>
-      <div className="mx-auto ">
+      <div className="flex items-center justify-center mt-5">
         <Button size="visit" bgColor="Yellow" text="See More" goto="/gallery" />
+      </div>
       </div>
     </div>
   );
