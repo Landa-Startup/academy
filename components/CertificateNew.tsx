17abeb36e53ecbb3cee0062c4e49530af0762786
فهیمه السادات سجادی <fahimehsjd77@gmail.com>
2024-01-16 15:14:32 +0330

    fix: fixed and responsive certificate page

--- components/CertificateNew.tsx
+++ components/CertificateNew.tsx
@@ -2,46 +2,64 @@ import Image from 'next/image';
 import React from 'react';
 
 export default function CertificateNew() {
-  return (
-    <div className="relative h-[600px] w-full border-[6px] border-[#80C555] px-4 text-xl md:flex md:items-center md:justify-around md:px-0">
-      <div className="flex h-full flex-col md:h-[30rem] md:justify-between">
-        <div className="my-10 flex h-[18rem] flex-col justify-between md:w-[30rem]">
-          <p className="text-lg">Aug 30.2023</p>
-          <p className="font-gilda text-2xl font-semibold md:text-3xl">
-            Ali Khavari
-          </p>
-          <p className="text-xl md:text-2xl">has successfully completed </p>
-          <p className="font-gilda text-3xl font-semibold md:text-2xl">
-            SEO Course
-          </p>
-          <p className="text-xl md:text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-            minus, corporis facilis omnis quaerat aliquam cum commodi
-            perferendis.
-          </p>
+    return (
+        <div
+            className="relative h-auto md:h-[100vh] text-xl flex md:items-center md:justify-around bg-[url('/static/images/Academy/certificate/certificate1.jpg')] bg-[length:100%_100%] bg-no-repeat">
+            <div className="flex h-full flex-col md:h-[30rem] md:justify-between my-10 md:ms-48 ps-5">
+                <div className="flex md:h-[18rem] flex-col justify-start md:justify-between md:w-[30rem] gap-y-2">
+                    <p className="text-sm md:text-lg">Aug 30.2023</p>
+                    <p className="font-gilda text-md font-semibold md:text-3xl">
+                        Ali Khavari
+                    </p>
+                    <p className="text-sm md:text-2xl">has successfully completed </p>
+                    <p className="font-gilda text-md font-semibold md:text-2xl">
+                        SEO Course
+                    </p>
+                    <p className="text-sm md:text-2xl">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+                        minus, corporis facilis omnis quaerat aliquam cum commodi
+                        perferendis.
+                    </p>
+                </div>
+                <div className="w-1/2 md:w-full">
+                    <Image
+                        className="mt-5"
+                        loading="lazy"
+                        src={`/static/images/Academy/certificate/text.png`}
+                        alt="flag"
+                        width={200}
+                        height={100}
+                    />
+                </div>
+            </div>
+            <div className="flex flex-col items-center justify-between ">
+                <div
+                    className="relative top-0 flex flex-col items-center justify-around pb-4 h-[130px] w-[70px] md:h-96 md:w-64 p-1 bg-[url('/static/images/Academy/certificate/flag.png')] bg-cover md:bg-contain bg-no-repeat bg-center">
+                    <Image
+                        loading="lazy"
+                        src={`/static/images/Academy/certificate/QR_Code_Example.svg`}
+                        alt="flag"
+                        width={150}
+                        height={150}
+                    />
+                    <Image
+                        loading="lazy"
+                        src={`/static/images/Academy/certificate/TEXT2.png`}
+                        alt="flag"
+                        width={150}
+                        height={100}
+                    />
+                </div>
+                <div>
+                    <Image
+                        loading="lazy"
+                        src={`/static/images/Academy/Logo.svg`}
+                        alt="Landa"
+                        width={400}
+                        height={400}
+                    />
+                </div>
+            </div>
         </div>
-        <div className="flex w-[10rem] flex-col items-center text-xs md:text-sm">
-          <p className="border-b-2">RASOUL MORADIMEHR</p>
-          <p>Managing Director</p>
-        </div>
-      </div>
-      <div className="w-[50rem]">
-        <div className="absolute right-9 top-0 flex h-24 flex-col items-center justify-end bg-[#CCE8BB] pb-4 md:right-32 md:h-48 md:w-44">
-          <p className="text-sm md:text-3xl">CERTIFICATE</p>
-          <p className="text-xs md:text-base">OF PARTICIPATION</p>
-        </div>
-        {/* logo */}
-        <div>
-          <Image
-            loading="lazy"
-            className="absolute bottom-0 right-0 md:right-3"
-            src={`/static/images/Academy/Logo.svg`}
-            alt="Landa"
-            width={100}
-            height={100}
-          />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
